9c4d9b8f10e3007e79aeca1bc9441756ff176da7
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-11-13 23:25:59 +0530

    Added chat column

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,34 +1,48 @@
 import React from "react";
 import "../container/container.css";
+import Chat from "./Chat";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PersonAddIcon from "@mui/icons-material/PersonAddAlt";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
+import ModeNightIcon from "@mui/icons-material/ModeNight";
+import SearchIcon from "@mui/icons-material/Search";
 import { IconButton } from "@mui/material";
 
 const Sidebar = () => {
-  const icon = {
-    fontSize: 40,
-  };
-
   return (
     <div className="sidebar-container">
-      <div className="header">
-        <IconButton style={icon}>
-          <AccountCircleIcon />
-        </IconButton>
-        <IconButton>
-          <PersonAddIcon />
-        </IconButton>
-        <IconButton>
-          <GroupAddIcon />
-        </IconButton>
-        <IconButton>
-          <AddCircleIcon />
-        </IconButton>
+      <div className="sb-header">
+        <div>
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </div>
+        <dir>
+          <IconButton>
+            <PersonAddIcon />
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton>
+            <ModeNightIcon />
+          </IconButton>
+        </dir>
       </div>
-      <div className="search">search</div>
-      <div className="chat">chat</div>
+      <div className="sb-search">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+
+        <input type="search" placeholder="search" className="search-box" />
+      </div>
+      <div className="sb-chat">
+        <Chat />
+      </div>
     </div>
   );
 };
