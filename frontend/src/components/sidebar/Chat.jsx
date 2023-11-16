3ed487a2f05e92183c47bdf1568734f54af5117f
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-11-16 18:18:37 +0530

    Added chat details

--- frontend/src/components/sidebar/Chat.jsx
+++ frontend/src/components/sidebar/Chat.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Chat = () => {
-  return <div>chat goes here</div>;
+  return (
+    <div className="chat-container">
+      <p className="chat-icon">{props.name[0]}</p>
+      <p className="chat-title">{props.name}</p>
+      <p className="chat-lastMessage">{props.lastMessage}</p>
+      <p className="chat-timeStamp">{props.timeStamp}</p>
+    </div>
+  );
 };
 
 export default Chat;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../container/container.css";
 import Chat from "./Chat";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -10,6 +10,23 @@ import SearchIcon from "@mui/icons-material/Search";
 import { IconButton } from "@mui/material";
 
 const Sidebar = () => {
+  const [chats, setChats] = useState([
+    {
+      name: "Test1",
+      lastMessage: "Last message 1",
+      timeStamp: "today",
+    },
+    {
+      name: "Test2",
+      lastMessage: "Last message 2",
+      timeStamp: "today",
+    },
+    {
+      name: "Test3",
+      lastMessage: "Last message 3",
+      timeStamp: "today",
+    },
+  ]);
   return (
     <div className="sidebar-container">
       <div className="sb-header">
@@ -41,7 +58,9 @@ const Sidebar = () => {
         <input type="search" placeholder="search" className="search-box" />
       </div>
       <div className="sb-chat">
-        <Chat />
+        {chats.map((chats) => {
+          return <Chat props={chats} />;
+        })}
       </div>
     </div>
   );
